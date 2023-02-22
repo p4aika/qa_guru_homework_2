885d1fde5d48603765f870d8cff9a83f26bfec8b
Pavel Chaika <mrpavel.chaika@gmail.com>
2023-02-22 11:33:21 +0300

    fix_homework2

--- qa_guru_homework_2.py
+++ qa_guru_homework_2.py
@@ -8,12 +8,12 @@ def set_browser_size():
     browser.config.window_width = 1900
     browser.config.window_height = 1080
 
-def test_google_should_finde_text(set_browser_size):
+def test_google_should_find_text(set_browser_size):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
-def test_google_shuld_not_finde_text():
+def test_google_should_not_find_text(set_browser_size):
     browser.open('https://google.com')
-    browser.element('[name="q"]').should(be.blank).type('testgoogleshuldnotfindetext').press_enter()
-    browser.element('#topstuff').should(have.text('По запросу testgoogleshuldnotfindetext ничего не найдено.'))
+    browser.element('[name="q"]').should(be.blank).type('testgoogleshouldnotfindtext').press_enter()
+    browser.element('#topstuff').should(have.text('По запросу testgoogleshouldnotfindtext ничего не найдено.'))
